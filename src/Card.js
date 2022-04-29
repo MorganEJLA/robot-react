3114fe78a9477154563f0de066b4cfd475e83aee
Morgan J <40442819+MorganEJLA@users.noreply.github.com>
2022-04-29 15:01:25 -0400

    first commit, starter files

---
+++ src/Card.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Card = () => {
+    return(
+        <div>
+            <img alt = "artists" src='https://robohash.org/test?200X200'/>
+       
+            <div>
+                <h2>Nampeyo</h2>
+                <p>nampeyo@example.com</p>
+
+            </div>
+        </div>
+    );
+}
+
+export default Card;
